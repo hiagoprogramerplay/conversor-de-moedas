dbb383e85bd003e8c1107c8afff891e07169bd6c
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-03-05 21:11:30 -0300

    alteração da primeira etapa do conversor

---
+++ cad.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONVERSOR DE MOEDAS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <strong><h1>CONVERSOR DE MOEDAS</h1></strong>
+    
+</body>
+</html>
